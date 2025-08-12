1ae52bd3056c1d1e26a94adafcf1426f1e700878
Petrov Andrey <4ndrew1337@mail.ru>
2025-08-12 17:59:09 +0300

    add html form for order search

--- internal/controller/http/v1/orders.go
+++ internal/controller/http/v1/orders.go
@@ -1,7 +1,10 @@
 package v1
 
 import (
+	"encoding/json"
+	"html/template"
 	"net/http"
+	"path/filepath"
 
 	"github.com/gofiber/fiber/v2"
 )
@@ -9,6 +12,20 @@ import (
 func (r *V1) order(ctx *fiber.Ctx) error {
 	orderUID := ctx.Query("order_uid")
 
+	// path to html files
+	tmplPath := filepath.Join("docs", "html")
+
+	// show `order search`
+	if orderUID == "" {
+		t, err := template.ParseFiles(filepath.Join(tmplPath, "order_form.html"))
+		if err != nil {
+			return errorResponse(ctx, http.StatusInternalServerError, err.Error())
+		}
+		ctx.Type("html")
+		return t.Execute(ctx.Response().BodyWriter(), nil)
+	}
+
+	// get order
 	order, err := r.o.Order(ctx.UserContext(), orderUID)
 	if err != nil {
 		r.l.Error(err, "http - v1 - order")
@@ -16,5 +33,17 @@ func (r *V1) order(ctx *fiber.Ctx) error {
 		return errorResponse(ctx, http.StatusInternalServerError, "storage problems")
 	}
 
-	return ctx.Status(http.StatusOK).JSON(order)
+	pretty, _ := json.MarshalIndent(order, "", "  ")
+
+	// show `order info`
+	t, err := template.ParseFiles(filepath.Join(tmplPath, "order_info.html"))
+	if err != nil {
+		return errorResponse(ctx, http.StatusInternalServerError, err.Error())
+	}
+	data := map[string]interface{}{
+		"Order":      order,
+		"PrettyJSON": string(pretty),
+	}
+	ctx.Type("html")
+	return t.Execute(ctx.Response().BodyWriter(), data)
 }
